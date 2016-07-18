f2766fcd7e600b754cc03878ef1d642dd9e6804a
Karen <karenjxliu@hotmail.com>
2016-07-18 16:09:19 -0500

    completed hotel_reservation exercise

--- algo/testing.rb
+++ algo/testing.rb
@@ -1,24 +1,68 @@
-def sum_of_range(array)
-  array = array.sort
-  num_of_numbers = array.last - array.first
-  current_number = array.first
-  sum = 0
-  (num_of_numbers + 1).times do
-    sum = sum + current_number 
-    current_number = current_number + 1
+class HotelReservation
+  attr_accessor :customer_name, :date, :room_number, :amenities
+
+  def initialize(reservation_info)
+    @customer_name = reservation_info[:customer_name]
+    @date = reservation_info[:date]
+    @room_number = reservation_info[:room_number]
+    @amenities = []
   end
-  puts "next number:"
-  puts current_number
-  puts "sum:"
-  puts sum
+
+  def add_a_fridge
+    @amenities << "fridge"
+  end
+
+  def add_a_crib
+    @amenities << "crib"
+  end
+
+  def add_a_custom_amenity(amenity)
+    @amenities << amenity
+  end
 end
 
+puts 'Testing code'
+puts
 
-result = sum_of_range([1, 4])
-puts "here's the result:"
+reservation = HotelReservation.new(customer_name: "John", date: "16-07-18", room_number: "300")
+p reservation
+
+reservation.room_number= "400"
+result = reservation.room_number
 puts result
+if result == "400"
+  puts "PASS"
+else 
+  puts "Please inspect the writer method for room number."
+end
 
-result = sum_of_range([2, 8])
-puts "here's the result:"
-puts result
+reservation.add_a_fridge
+result = reservation.amenities
+p reservation.amenities
+if result.include? "fridge"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_fridge method."
+end
 
+reservation.amenities = []
+
+reservation.add_a_crib
+result = reservation.amenities
+p reservation.amenities
+if result.include? "crib"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_crib method."
+end  
+
+reservation.amenities = []
+
+reservation.add_a_custom_amenity("microwave")
+result = reservation.amenities
+p reservation.amenities
+if result.include? "microwave"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_custom_amenity method."
+end  
--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -32,3 +32,49 @@ end
 # The add_a_crib method
 # The add_a_custom_amenity method
 
+
+puts 'Testing code'
+puts
+
+reservation = HotelReservation.new(customer_name: "John", date: "16-07-18", room_number: "300")
+p reservation
+
+reservation.room_number= "400"
+result = reservation.room_number
+puts result
+if result == "400"
+  puts "PASS"
+else 
+  puts "Please inspect the writer method for room number."
+end
+
+reservation.add_a_fridge
+result = reservation.amenities
+p reservation.amenities
+if result.include? "fridge"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_fridge method."
+end
+
+reservation.amenities = []
+
+reservation.add_a_crib
+result = reservation.amenities
+p reservation.amenities
+if result.include? "crib"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_crib method."
+end  
+
+reservation.amenities = []
+
+reservation.add_a_custom_amenity("microwave")
+result = reservation.amenities
+p reservation.amenities
+if result.include? "microwave"
+  puts "PASS"
+else
+  puts "Please inspect the add_a_custom_amenity method."
+end  
